985d2f7c6e8af166da53da09635357e86433cbd8
Abdul Rahman Kayali <abdkayali@outlook.com>
2017-03-31 00:44:02 +0300

    Added a simple test

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,8 @@
 import sensitiveWords from '.'
 
-test('says hello world', () => (
-    expect(sensitiveWords()).toBe('Hello World')
+let expected = 'The constant in software is ***';
+let originalText = 'The constant in software is change';
+test('replaces sensitive words with 3 asterisk', 
+    () => (
+    expect(sensitiveWords(originalText, ['change'])).toBe(expected)
 ))
